fe4b4b7dbb5faf48419de5565e4812a2037ebda3
cdcsccc <468027952@qq.com>
2025-10-20 13:42:34 -0400

    Finish -[ ] TODO: save the logout controller in the instance variable. -[ ] TODO: execute the logout use case through the Controller

--- src/main/java/view/LoggedInView.java
+++ src/main/java/view/LoggedInView.java
@@ -4,6 +4,7 @@ import interface_adapter.logged_in.ChangePasswordController;
 import interface_adapter.logged_in.LoggedInState;
 import interface_adapter.logged_in.LoggedInViewModel;
 import interface_adapter.logout.LogoutController;
+import interface_adapter.signup.SignupState;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
@@ -109,6 +110,7 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
      */
     public void actionPerformed(ActionEvent evt) {
         System.out.println("Click " + evt.getActionCommand());
+        logoutController.execute();
     }
 
     @Override
@@ -139,6 +141,6 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
     }
 
     public void setLogoutController(LogoutController logoutController) {
-        // TODO: save the logout controller in the instance variable.
+        this.logoutController = logoutController;
     }
 }
